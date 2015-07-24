7ab20ac39a712a7e5bd21f40c503e62b26b232a3
Vagrant User <vagrant@Wheezy-64.vagrantup.com>
2015-07-24 13:33:44 +0000

    fix swagger-editor dev port

--- swagger-editor/docker.sh
+++ swagger-editor/docker.sh
@@ -3,5 +3,5 @@
 DIR=$(pwd)
 echo $DIR
 sudo docker build $1 -t mike/swagger-editor .
-sudo docker run -t -i -v $DIR:/opt/ -p 3000:3000  mike/swagger-editor /opt/run.sh
+sudo docker run -t -i -v $DIR:/opt/docker -p 3000:3000  mike/swagger-editor /opt/docker/run.sh
 
--- swagger-editor/run.sh
+++ swagger-editor/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd /opt/swagger/ && NODE_PATH=/opt/node  PORT=3000 grunt serve
+cd /opt/swagger/ && NODE_PATH=/opt/node && PATH=$PATH:/opt/node/bin  PORT=3000 grunt serve
